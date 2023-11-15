d1bc75b653f9536f60d89b2b8e7e0993e322bf8e
Gil Cohen <cohengil333@gmail.com>
2023-11-15 22:41:43 +0200

    Example works with sqlite

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from fastapi import FastAPI, Request, Response, Form, status, Depends
+from fastapi import FastAPI, Depends, Request, Form, status
 from starlette.responses import RedirectResponse
 from starlette.templating import Jinja2Templates
 from sqlalchemy.orm import Session
-
 import models
-from database import SessionLocal,engine
+from database import SessionLocal, engine
+import uvicorn
 
-templates = Jinja2Templates(directory='templates')
+models.Base.metadata.create_all(bind=engine)
+templates = Jinja2Templates(directory="templates")
 
 app = FastAPI()
 
@@ -20,10 +21,42 @@ def get_db():
         db.close()
 
 
-@app.get('/')
+@app.get("/")
 def home(request: Request, db: Session = Depends(get_db)):
     todos = db.query(models.Todo).all()
-    return templates.TemplateResponse('base.html',
-                                      {'request': request, 'todo_list': todos})
+    return templates.TemplateResponse("base.html",
+                                      {"request": request, "todo_list": todos})
 
 
+@app.post("/add")
+def add(request: Request, title: str = Form(...), db: Session = Depends(get_db)):
+    new_todo = models.Todo(title=title)
+    db.add(new_todo)
+    db.commit()
+
+    url = app.url_path_for("home")
+    return RedirectResponse(url=url, status_code=status.HTTP_303_SEE_OTHER)
+
+
+@app.get("/update/{todo_id}")
+def update(request: Request, todo_id: int, db: Session = Depends(get_db)):
+    todo = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
+    todo.complete = not todo.complete
+    db.commit()
+
+    url = app.url_path_for("home")
+    return RedirectResponse(url=url, status_code=status.HTTP_302_FOUND)
+
+
+@app.get("/delete/{todo_id}")
+def delete(request: Request, todo_id: int, db: Session = Depends(get_db)):
+    todo = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
+    db.delete(todo)
+    db.commit()
+
+    url = app.url_path_for("home")
+    return RedirectResponse(url=url, status_code=status.HTTP_302_FOUND)
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=8000)
--- database.py
+++ database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./db.sqlite'
+# dialect+driver://username:password@host:port/database
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./my_db.sqlite'
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False}
@@ -11,4 +12,3 @@ engine = create_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
